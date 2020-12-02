5656d8c161f09c13978bbe4ce3f203b65588eba4
dvranga <devangmranganath@gmail.com>
2020-12-02 09:48:18 +0530

    [ranganath]Added:Ability to edit the contact in the address book

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.addressbook;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +7,7 @@ import java.util.Scanner;
 public class AddressBook {
 
     static Scanner sc = new Scanner(System.in);
-    static List<AddressBookDetails> lst = new ArrayList<AddressBookDetails>();
+    static List<AddressBookDetails> addressbookList = new ArrayList<AddressBookDetails>();
     AddressBookDetails addContact;
 
     public void addContact() {
@@ -29,8 +28,65 @@ public class AddressBook {
         System.out.println("Enter your emailId : ");
         String email = sc.nextLine();
         addContact = new AddressBookDetails(firstName, lastName, address, city, state, zip, phoneNo, email);
-            lst.add(addContact);
+            addressbookList.add(addContact);
+        System.out.println(addressbookList);
 
     }
 
+    public void editContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter the first name ");
+        String firstNameToCheck = scanner.nextLine();
+        for (int index = 0; index < addressbookList.size(); index++) {
+            if (addressbookList.get(index).getFirstName().equals(firstNameToCheck)) {
+                System.out.println(addressbookList.get(index));
+                Scanner updateContact = new Scanner(System.in);
+                System.out
+                        .println(" Enter a choice 	1.first name 2.last name 3. city 4.state 5.zip 6.phone 7.email ");
+                int selection = scanner.nextInt();
+                switch (selection) {
+                    case 1:
+                        System.out.println(" Enter first name ");
+                        String first_Name = updateContact.nextLine();
+                        addressbookList.get(index).setFirstName(first_Name);
+                        System.out.println(addressbookList.get(index).getFirstName());
+                        break;
+                    case 2:
+                        System.out.println(" Enter last name ");
+                        String second_Name = updateContact.nextLine();
+                        addressbookList.get(index).setLastName(second_Name);
+                        break;
+                    case 3:
+                        System.out.println(" Enter city name ");
+                        String input_City = updateContact.nextLine();
+                        addressbookList.get(index).setCity(input_City);
+                        break;
+                    case 4:
+                        System.out.println(" Enter State ");
+                        String input_State = updateContact.nextLine();
+                        addressbookList.get(index).setCity(input_State);
+                        break;
+                    case 5:
+                        System.out.println(" Enter pincode ");
+                        String input_Zip = updateContact.nextLine();
+                        addressbookList.get(index).setCity(input_Zip);
+                        break;
+                    case 6:
+                        System.out.println(" Enter Mobile number ");
+                        String input_Phone = updateContact.nextLine();
+                        addressbookList.get(index).setZip(input_Phone);
+                        break;
+                    case 7:
+                        System.out.println(" Enter Email id ");
+                        String input_Email = updateContact.nextLine();
+                        addressbookList.get(index).setCity(input_Email);
+                        break;
+                    default:
+                        System.out.println(" Enter valid input ");
+                        break;
+                }
+            }
+        }
+        System.out.println(addressbookList);
+    }
 }
--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -9,24 +9,33 @@ public class AddressBookMain {
     }
 
 
-    public static void updateAddressBook() throws IOException {
+    public static void updateAddressBook() {
         AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
         int count=1;
         while (count==1) {
             System.out.println("Welcome to address book program ");
             System.out.println(
-                    "Enter choice \n1. AddContact ");
+                    "Enter choice \n1. AddContact  \n2.Edit ");
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
                     addressBook.addContact();
                     count=0;
                     break;
+                case 2:
+                    if (AddressBook.addressbookList.isEmpty()) {
+                        System.out.println(" Address book is empty ");
+                        break;
+                    }
+                    addressBook.editContact();
+                    count=0;
+                    break;
                 default:
                     System.out.println(" Enter a valid choice");
                     break;
             }
+            count=0;
         }
     }
 }
